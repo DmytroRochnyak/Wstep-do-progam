a140485555e4d58d502bcc5e7e5b6003cc8b5b00
w68167 <rochyack.dmytro@gmail.com>
2022-11-03 15:06:16 +0100

    zadanie 1 - 3 (lab 2)

---
+++ 3.7 lab.py
@@ -0,0 +1,7 @@
+a = int(input())
+
+b = "*"
+for k in range(a):
+   for i in range(a):
+    print(b, end = " ")
+   print("")
--- zadanie.py
+++
@@ -1,5 +0,0 @@
-#grupa 1
-a = int(input("Wpisz pierwsze liczb"))
-b = int(input("Wpisz druge liczbe"))
-P = a ** b
-print ('Wynik podniesienia pierwszej liczby do potęgi, którą jest druga liczba =', P)
